a2a4c6336b79a0d2b096659e00b3e90d3d01c036
CHITHU00 <114564009+CHITHARANJAN-T@users.noreply.github.com>
2022-12-13 10:18:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Package from './components/Package'
+import './styles/basic.css'
+
+	const root = ReactDOM.createRoot(document.querySelector('.subContainer'));
+	root.render(<Package />);
